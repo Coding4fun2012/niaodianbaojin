b72b6d569519db7db24cebc53e3e2a9f5c16d62a
Coding4fun2012 <ljmasdf110@gmail.com>
2023-09-09 18:03:50 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    music.stopAllSounds()
+})
 radio.onReceivedString(function (receivedString) {
+    basic.showIcon(IconNames.Heart)
     if (receivedString == "A") {
         music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
     }
     if (receivedString == "B") {
-        music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.LoopingInBackground)
     }
 })
 radio.setGroup(1)
